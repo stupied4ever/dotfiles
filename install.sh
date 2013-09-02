f384642f84af0d769011c5ed9467561848bd8a76
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-09-02 12:47:24 -0300

    Fix .oh-my-zsh to use mine

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ fi
 
 if [ ! -e "$HOME/.oh-my-zsh" ]; then
   mkdir -p "$HOME/.oh-my-zsh"
-  git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
+  https://github.com/stupied4ever/oh-my-zsh "$HOME/.oh-my-zsh"
 fi
 
 vim +BundleInstall +qa! && clear && echo "Done! :)"
